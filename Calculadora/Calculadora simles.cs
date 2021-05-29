ec3110875e885c13ec70550c1718eca7df2352ec
Iury Oliveira <iuryffoliveira@yahoo.com>
2021-05-29 19:20:24 -0300

    Calculadora Teste

---
+++ Calculadora/Calculadora simles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro número: ");
+            int first = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo número: ");
+            int second = Int32.Parse(Console.ReadLine());
+            int result = first + second;
+            Console.WriteLine(first + " + " + second + " = " + result);
+            result = first - second;
+            Console.WriteLine(first + " - " + second + " = " + result);
+            result = first / second;
+            Console.WriteLine(first + " / " + second + " = " + result);
+            result = first * second;
+            Console.WriteLine(first + " * " + second + " = " + result);
+        }
+    }
+}
