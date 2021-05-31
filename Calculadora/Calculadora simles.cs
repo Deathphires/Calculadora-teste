1b359a16a8e7a0c7fdc178706cfa3c1eb5da238d
Iury Oliveira <iuryffoliveira@yahoo.com>
2021-05-31 15:28:59 -0300

    Excluído mensagens do JS

--- Calculadora/Calculadora simles.cs
+++ Calculadora/Calculadora simles.cs
@@ -41,37 +41,3 @@ namespace Calculadora
         }
     }
 }
-
-//{
-    //var primeiroValor = Int32.Parse(Console.ReadLine());
-    //Console.WriteLine("Primeiro número: ");
-  //  var segundoValor = Int32.Parse(Console.ReadLine());
-//    Console.WriteLine("Segundo número: ");
-    //var resultado = primeiroValor / segundoValor;
-  //  var operacao = ("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração: ");
-//    if (operacao == 1)
-
-   // {
-     //   var resultado = primeiroValor / segundoValor;
-    //    Console.WriteLine(primeiroValor + " + " + segundoValor + " = " + resultado);
-   // }
-   // else if (operacao == 2)
-   // {
-    //    var resultado = primeiroValor * segundoValor;
-    //    Console.WriteLine(primeiroValor + " * " + segundoValor + " = " + resultado);
-   // }
-   // else if (operacao == 3)
-    //{
-    //    var resultado = primeiroValor + segundoValor;
-    //    Console.WriteLine(primeiroValor + " + " + segundoValor + " = " + resultado);
-    //}
-   // else if (operacao == 4)
-   // {
-    //    var resultado = primeiroValor - segundoValor;
-   //     Console.WriteLine(primeiroValor + " - " + segundoValor + " = " + resultado);
-  //  }
-//    else;
-    //{
-  //      Console.WriteLine("!Opção inválida!");
-//    }
-//}
