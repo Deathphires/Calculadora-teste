89c33bc98d98d780fa710421525972f584ac96d2
ypedroo <ynoa.pedro@outlook.com>
2021-06-04 19:00:04 -0300

    change it to method

---
+++ Calculadora/test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculadora
+{
+    public class test1
+    {
+        public float percentage(int number, int percentual)
+        {
+            var resultado = operacoes.Somar(number, 100);
+            return resultado / percentual * 100;
+        }
+    }
+}
