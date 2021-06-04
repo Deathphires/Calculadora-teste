fc1ee116cc045660cd82a6246f15b651c5248cc8
Iury Oliveira <iuryffoliveira@yahoo.com>
2021-06-03 21:08:04 -0300

    Alteração da Calculadora

--- Calculadora/Calculadora simles.cs
+++ Calculadora/Calculadora simles.cs
@@ -16,23 +16,25 @@ namespace Calculadora
             var first = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Segundo número: ");
             var second = Int32.Parse(Console.ReadLine());
-            var operacao = ("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração: ");
-            if (operacao = 1)
+            Console.WriteLine("Digite 1 para fazer uma soma, 2 para subtração, 3 para divisão e 4 para multiplicação: ");
+            var operacao = Console.ReadLine();
+            var x = second.ToString();
+            if (operacao == "1")
             {
                 var resultado = first + second;
                 Console.WriteLine(first + " + " + second + " = " + resultado);
             }
-            else if (operacao = 2)
+            else if (operacao == "2")
             {
                 var resultado = first - second;
                 Console.WriteLine(first + " - " + second + " = " + resultado);
             }
-            else if (operacao = 3)
+            else if (operacao == "3")
             {
                 var resultado = first / second;
                 Console.WriteLine(first + " / " + second + " = " + resultado);
             }
-            else if (operacao = 4)
+            else if (operacao == "4")
             {
                 var resultado = first * second;
                 Console.WriteLine(first + " * " + second + " = " + resultado);
