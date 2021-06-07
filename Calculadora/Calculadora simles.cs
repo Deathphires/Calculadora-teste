ceefa35e8ba494dc4e602b2f4da35026474b857a
Iury Oliveira <iuryffoliveira@yahoo.com>
2021-06-07 17:47:47 -0300

    Pressão da porra mas ficou bom

--- Calculadora/Calculadora simles.cs
+++ Calculadora/Calculadora simles.cs
@@ -16,7 +16,7 @@ namespace Calculadora
             var first = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Segundo número: ");
             var second = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite 1 para fazer uma soma, 2 para subtração, 3 para divisão e 4 para multiplicação: ");
+            Console.WriteLine("Digite 1 para fazer uma soma, 2 para subtração, 3 para divisão, 4 para multiplicação, 5 para raiz quadrada do primeiro valor e 6 para expoente: ");
             var operacao = Console.ReadLine();
             if (operacao == "1")
             {
@@ -37,6 +37,16 @@ namespace Calculadora
             {
                 var resultado = operacoes.Multiplicar(first, second);
                 Console.WriteLine(first + " * " + second + " = " + resultado);
+            }
+            else if (operacao == "5")
+            {
+                var resultado = operacoes.Raiz(first);
+                Console.WriteLine("raiz quadrada de " + first + " é =" + resultado); //num meche no que tá quieto
+            }
+            else if (operacao == "6")
+            {
+                var resultado = operacoes.Expoente(first, second);
+                Console.WriteLine(first + " elevado à " + second + "ª potência " + "é =" + resultado);
             }
         }
     }
--- Calculadora/operacoes.cs
+++ Calculadora/operacoes.cs
@@ -6,23 +6,38 @@ namespace Calculadora
     {
         public static int Somar(int primeiroValor, int segundoValor)
         {
-            return primeiroValor + segundoValor;
-            
+            var x = primeiroValor + segundoValor;
+            return x;
         }
 
         public static int Subtrair(int primeiroValor, int segundoValor)
         {
-            return primeiroValor - segundoValor;
+            var x = primeiroValor - segundoValor;
+            return x;
         }
 
         public static int Dividir(int primeiroValor, int segundoValor)
         {
-            return primeiroValor / segundoValor;
+            var x = primeiroValor / segundoValor;
+            return x;
         }
 
-        public static int Multiplicar(int primeiroValor, int segundoValor)
+        public static double Multiplicar(int primeiroValor, int segundoValor)
         {
-            return primeiroValor * segundoValor;
+            var x = primeiroValor * segundoValor;
+            return x;
+        }
+
+        public static double Raiz(int primeiroValor)
+        {
+            var x = Math.Sqrt(primeiroValor);
+            return x;
+        }
+
+        public static double Expoente(double primeiroValor, double segundoValor)
+        {
+            var x = Math.Pow(primeiroValor, segundoValor);
+            return x;
         }
     }
 }
